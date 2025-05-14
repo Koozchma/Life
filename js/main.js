45525047657d5a8b238fae7c4db1445d0b7381d6
Koozchma <131485358+Koozchma@users.noreply.github.com>
2025-05-14 17:26:32 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ document.getElementById('start-button').addEventListener('click', () => {
   document.getElementById('game').style.display = 'block';
   updateUI();
 });
+
+document.getElementById('click-button').addEventListener('click', () => {
+  gameState.money += gameState.incomePerClick;
+  updateUI();
+
+  // 10% chance to trigger random event
+  if (Math.random() < 0.1) {
+    triggerRandomEvent();
+  }
+});
+
+window.onload = () => {
+  updateUI();
+};
