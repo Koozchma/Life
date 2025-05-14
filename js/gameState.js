ff85746b077652ec37375cf2979ceeeed6d65c80
Koozchma <131485358+Koozchma@users.noreply.github.com>
2025-05-14 19:17:39 -0400

    Update gameState.js

--- js/gameState.js
+++ js/gameState.js
@@ -5,3 +5,13 @@ const gameState = {
   events: [],
   career: null,
 };
+
+const gameState = {
+  money: 0,
+  incomePerClick: 1,
+  properties: [],
+  events: [],
+  career: null,
+  gender: null,
+  path: null, // "education" or "career"
+};
