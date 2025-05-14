2a2f14846f14cec24b36ae3741c172d65b39a410
Koozchma <131485358+Koozchma@users.noreply.github.com>
2025-05-14 17:26:17 -0400

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,9 +1,12 @@
 function triggerRandomEvent() {
   const events = [
-    { description: "You won a scratch-off ticket!", value: +100 },
-    { description: "Car repair needed", value: -50 },
-    { description: "Bonus at work!", value: +75 },
+    { description: "🎉 You won a scratch-off ticket!", value: +100 },
+    { description: "🚗 Unexpected car repair", value: -50 },
+    { description: "💼 Bonus at work!", value: +75 },
+    { description: "🧾 Tax refund arrived", value: +120 },
+    { description: "📉 Stock market dip", value: -80 },
   ];
+
   const event = events[Math.floor(Math.random() * events.length)];
   gameState.money += event.value;
   gameState.events.push(event);
